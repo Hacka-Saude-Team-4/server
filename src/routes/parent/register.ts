16e2b84516968eb74e26df776f2d25a3bd086a2a
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-23 13:43:24 -0300

    feat: add bcrypt and hash user parent password before saving to database

--- src/routes/parent/register.ts
+++ src/routes/parent/register.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { hash } from 'bcrypt';
 import { Parent } from '../../entity/Parent';
 import { getRepository } from 'typeorm';
 
@@ -11,6 +12,7 @@ export default async (req: Request, res: Response) => {
 	try {
 		// Get data from request
 		const { email, password }: ParentCredentials = req.body;
+		const hashedPassword = await hash(password, 12);
 
 		// Check if user already exists
 		let parent = await Parent.findOne({ where: { email } });
@@ -19,19 +21,18 @@ export default async (req: Request, res: Response) => {
 			return res
 				.status(409)
 				.json({ msg: 'User already exists. Try signing in.' });
-		} else {
-			// Create a new instance of Parent
-			parent = new Parent();
-			parent.email = email;
-			parent.password = password;
-
-			// Save parent to DB
-			const parentsRepo = getRepository(Parent);
-			await parentsRepo.save(parent);
-			res.status(200).json({ msg: 'Parent added successfully.', parent });
 		}
 
-		// else: If user does not exist yet, create a new one
+		// If user does not exist yet
+		// create a new instance of Parent
+		parent = new Parent();
+		parent.email = email;
+		parent.password = hashedPassword;
+
+		// Save parent to DB
+		const parentsRepo = getRepository(Parent);
+		await parentsRepo.save(parent);
+		res.status(200).json({ msg: 'Parent added successfully.', parent });
 	} catch (err) {
 		console.log('Error at user register: ', err);
 		return res.status(500).json({ msg: 'Error at user register', err });
