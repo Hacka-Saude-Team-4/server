04d11baac9c0574c760d514213d81097e9015474
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-23 13:30:49 -0300

    chore: test DB insertion inside child route

--- src/routes/child/greet.ts
+++ src/routes/child/greet.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../../entity/User';
 
 export default async (req: Request, res: Response) => {
-	res.send('hiay there');
+	console.log('Inserting a new user into the database...');
+	const user = new User();
+	user.firstName = 'Timber';
+	user.lastName = 'Saw';
+	user.age = 25;
+
+	const userRepo = getRepository(User);
+	await userRepo.save(user);
+	console.log('Saved a new user with id: ' + user.id);
+
+	console.log('Loading users from the database...');
+	const users = await userRepo.find();
+	console.log('Loaded users: ', users);
+
+	res.send('User added successfully, here are all of them:');
 };
