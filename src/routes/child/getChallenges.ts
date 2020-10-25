68b3f6ddc84848c92275446e8ac46f4722c0ffa1
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-25 13:46:55 -0300

    feat: list all child challenges by ID

---
+++ src/routes/child/getChallenges.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { User } from '../../entity/User';
+import { getRepository } from 'typeorm';
+import { Challenge } from '../../entity/Challenge';
+
+export default async (req: Request, res: Response) => {
+	try {
+		// De-structure child ID
+		const { userId } = req.payload as JWTPayload;
+
+		// Get child challenges
+		const child = await User.findOne({
+			where: { id: userId },
+			relations: ['challenges'],
+		});
+
+		res.status(200).json({
+			msg: 'Listing all child challenges.',
+			challenges: child.challenges,
+		});
+	} catch (err) {
+		console.log('Error at listing challenges: ', err);
+		return res.status(500).json({ msg: 'Error at add challenge', err });
+	}
+};
--- src/routes/child/index.ts
+++ src/routes/child/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import validateAuth from '../../middleware/validateAuth';
+import getChallenges from './getChallenges';
 import login from './login';
 
 const childRoutes = Router();
 
 childRoutes.post('/login', login);
+childRoutes.get('/challenges', validateAuth, getChallenges);
 
 export default childRoutes;
